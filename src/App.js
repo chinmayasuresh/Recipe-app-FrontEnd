68c71198b7e91bcd4075956da13eb161af8245a4
chinmayasuresh <chinmayasuresh12@gmail.com>
2024-04-09 19:03:07 +0530

    search  Recipe Completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import AddRecipe from './components/AddRecipe';
+import SearchRecipe from './components/SearchRecipe';
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
 
 
 <AddRecipe/>
-
+<SearchRecipe/>
 </div>
   );
 }
