d4de27fe2d2f5c5ea76e475f0796f779005b38c4
chinmayasuresh <chinmayasuresh12@gmail.com>
2024-04-09 22:26:11 +0530

    Completed

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import SearchRecipe from './components/SearchRecipe';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import View from './components/View';
+import Main from './components/Main';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
 <NavBar/>
 <BrowserRouter>
 <Routes>
+<Route path="/" element={<Main/>}   />
 <Route path="/view" element={<View/>}   />
 <Route path="/search" element={<SearchRecipe/>}   />
 <Route path="/add" element={<AddRecipe/>}   />
---
+++ src/components/Main.jsx
@@ -0,0 +1,94 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div>
+
+<div className="container">
+    <div className="row">
+    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+    <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img height="600px"src="https://imgsrv2.voi.id/liLqUL10KJ0F0CSjxYe950RHpp9j0ne6uymSkHKTjxk/auto/1200/675/sm/1/bG9jYWw6Ly8vcHVibGlzaGVycy8xMTUyOTcvMjAyMTEyMTgxNzU3LW1haW4uY3JvcHBlZF8xNjM5ODI1MDc3LmpwZw.jpg" class="d-block w-100" alt="..."></img>
+    </div>
+    <div class="carousel-item">
+      <img height="600px" src="https://img.freepik.com/free-photo/top-view-table-full-delicious-food-composition_23-2149141352.jpg?size=626&ext=jpg&ga=GA1.1.87170709.1707523200&semt=ais" class="d-block w-100" alt="..."></img>
+    </div>
+    <div class="carousel-item">
+      <img height="600px" src="https://itsmirchi.com.au/wp-content/uploads/Mirchi-Full-Range-Spread.jpg" class="d-block w-100" alt="..."></img>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+</div>
+</div>
+</div><br/>
+
+<div class="p-3 mb-2 bg-black text-white"><b>TOP SELLERS</b></div>
+
+<div className="row">
+    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+    <div className="row g-3">
+        <div className="col col-12 col-sm-3 col-md-3 col-lg-4 col-xl-3 col-xxl-3">
+        <div class="card" >
+  <img height="400px"src="https://loveincorporated.blob.core.windows.net/contentimages/gallery/3be0b2ab-1932-4ced-bc80-b684e9e3f6d7-12-pad-thai.jpg" class="card-img-top" alt="https://m.media-amazon.com/images/M/MV5BNGViM2M4NmUtMmNkNy00MTQ5LTk5MDYtNmNhODAzODkwOTJlXkEyXkFqcGdeQXVyMTY1NDY4NTIw._V1_.jpg"></img>
+  <div class="card-body">
+    
+  </div>
+</div>
+</div>
+<div className="col col-12 col-sm-3 col-md-3 col-lg-4 col-xl-3 col-xxl-3">
+        <div class="card" >
+  <img height="400px"src="https://loveincorporated.blob.core.windows.net/contentimages/gallery/507ef705-d362-441a-839e-18e9251ca1cd-33-bibimbap.jpg" class="card-img-top" alt="https://m.media-amazon.com/images/M/MV5BNGViM2M4NmUtMmNkNy00MTQ5LTk5MDYtNmNhODAzODkwOTJlXkEyXkFqcGdeQXVyMTY1NDY4NTIw._V1_.jpg"></img>
+  <div class="card-body">
+   
+  </div>
+</div>
+</div>
+<div className="col col-12 col-sm-3 col-md-3 col-lg-4 col-xl-3 col-xxl-3">
+        <div class="card" >
+  <img height="400px"src="https://www.vikhrolicucina.com/uploads/stories/1641453929_deepfriedfhickenrolldarksurface.jpg"></img>
+  <div class="card-body">
+  
+  </div>
+</div>
+</div>
+<div className="col col-12 col-sm-3 col-md-3 col-lg-4 col-xl-3 col-xxl-3">
+        <div class="card" >
+  <img height="400px"src="https://media.cnn.com/api/v1/images/stellar/prod/140430115517-06-comfort-foods.jpg?q=w_1280,h_720,x_0,y_0,c_fill"></img>
+  <div class="card-body">
+    
+  </div>
+</div>
+</div>
+
+</div>
+</div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+  )
+}
+
+export default Main
